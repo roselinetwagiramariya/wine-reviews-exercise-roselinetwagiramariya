007af6e936bcec5d8e108a7046c9bc12d65c5fa7
Roseline Twagiramariya <157075654+roselinetwagiramariya@users.noreply.github.com>
2024-05-21 18:19:00 -0400

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -1,2 +1,18 @@
-# add your code here
+import pandas as pd
+
+wine_data = pd.read_csv('data/winemag-data-130k-v2.csv.zip')
+
+
+summary_data = wine_data.groupby('country').agg({
+    'country': 'count',
+    'points': 'mean'
+}).rename(columns={'country': 'count', 'points': 'average_points'}).reset_index()
+
+
+summary_data['average_points'] = summary_data['average_points'].round(1)
+
+summary_data.to_csv('data/reviews-per-country.csv', index=False)
+
+print("Summary data saved successfully!")
+
 
